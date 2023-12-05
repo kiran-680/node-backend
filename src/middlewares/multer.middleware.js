f928125e54ef41be20fc7af3d20ce674ea58c7b6
Kiran <kopilakhadka122@gmail.com>
2023-12-05 16:37:12 +1100

    setup of midllewares

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+// Memory storage vs disk Storage 
+import multer from "multer"
+
+// configuring the storage of file 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+  export  {upload}
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,46 @@
+// we get file via file system : -
+//via local path i.e in the server 
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+
+// fs - link // unlink
+
+
+const uploadOnCloudinary=async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+
+        // upload the file on the cloudinary 
+
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file has been uploaded succesfully : -
+        console.log("File is uploaded on cloudinary")
+        return response;
+    } catch (error) {
+        // remove the locally saved temporary file as the upload operation got failed 
+        fs.unlinkSync(localFilePath);
+        return null;
+        
+    }
+
+}
+
+
+// // uploader 
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" }, 
+//   function(error, result) {console.log(result); });
+
+export {uploadOnCloudinary};
